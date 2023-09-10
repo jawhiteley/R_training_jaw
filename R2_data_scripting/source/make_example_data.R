8a6270d59174e3345cc914bb3230081835362a7f
Jonathan A. Whiteley <jawhiteley@me.com>
2023-09-10 00:24:06 -0400

    Add code to clean data to script that creates it
    + test that the cleaned data accurately reproduces
      the original (or at least, the values)
    - useful for planning demos, and their order

--- R2_data_scripting/source/make_example_data.R
+++ R2_data_scripting/source/make_example_data.R
@@ -1,11 +1,11 @@
 ################################################################
 ### Preparing an example file for the Workshop
 ### "A Short Introduction to Working With Data in R"
-### Jonathan Whiteley            R v4.1.3            2023-08-31
+### Jonathan Whiteley            R v4.3.1            2023-09-10
 ################################################################
 ## Using the `CO2` data set included in R
-## with a few extra modifications to illustrate 
-## dplyr and tidyr functions.
+## with a few modifications to illustrate 
+## dplyr, tidyr, and other tidyverse functions.
 
 ################################################################
 ### SETUP & LOAD
@@ -128,13 +128,98 @@ if (T) {
 }
 
 
-##==============================================================
-## Test
 
-test1 <- read.csv(file.path(out_path, "data_example.csv"), 
+
+################################################################
+### TEST - load & clean
+
+##==============================================================
+## Load
+
+test_base <- read.csv(file.path(out_path, "data_example.csv"), 
                   fileEncoding = "UTF-8", 
                   skip = 2#,
                   #na.strings = ""
                   )
 
-test2 <- readr::read_csv(file.path(out_path, "data_example.csv"), skip = 2)
+test_readr <- readr::read_csv(file.path(out_path, "data_example.csv"), skip = 2)
+
+
+##==============================================================
+## Clean
+
+## Use this for testing
+messy <- test_base
+
+## Clean `Type` column
+test_clean <- messy %>% 
+  mutate(Type = ifelse(Type == "Mississippi", Type, "Quebec"),  # In workshop, keep accent to make point about file encoding
+         Type = factor(Type, levels = unique(Type))
+  )
+str(test_clean)
+
+## Clean `Treatment` column
+test_clean <- test_clean %>% 
+  mutate(Treatment = na_if(Treatment, "")) %>%      # replace empty strings with NA
+  tidyr::fill(Treatment, .direction = "down") %>%   # Fill Down to replace NAs (tidyr)
+  mutate(Treatment = factor(Treatment, levels = unique(Treatment)))  # make it a factor
+str(test_clean)
+
+## The dataset is at least now able to be sorted and grouped without problems. :)
+
+## Recover Plant ID
+test_clean <- test_clean %>% 
+  mutate(
+    Type.tmp = str_sub(Type, 1, 1),
+    Treatment.tmp = str_sub(Treatment, 1, 1)
+  ) %>% 
+  unite(Plant, Type.tmp, Treatment.tmp, PlantNum, sep = "") %>% 
+  mutate(Plant = factor(Plant, levels = unique(Plant))) %>% 
+  relocate(Plant, Type, Treatment)
+str(test_clean)
+
+## Fix `500` column & convert to numeric
+test_clean <- test_clean %>% 
+  mutate(
+    X500 = str_split_i(X500, " ", i=1),  # drop everything after the first space
+    # X500 = str_replace(X500, fixed("+"), ""),    # strip extra character (not strictly necessary)
+    X500 = as.numeric(X500)
+  )
+str(test_clean)
+
+## Fix `675` column & convert to numeric
+test_clean <- test_clean %>% 
+  mutate(X675 = str_replace(X675, ",", ".") %>% as.numeric())
+str(test_clean)
+
+
+## Combine duplicate rows with summarize()
+## - in the workshop, explore functions like distinct() and duplicated() before doing this.
+test_clean <- test_clean %>% 
+  group_by(Plant, Type, Treatment) %>% 
+  summarize( across(where(is.numeric), ~ max(., na.rm = TRUE)), 
+             .groups = "drop"      # drop groups to avoid having to use ungroup()
+  )
+str(test_clean)  # now a 'tibble'
+
+
+## Reshape
+test_tidy <- test_clean %>% 
+  pivot_longer(
+    cols = where(is.numeric),    # or starts_with("X")
+    names_to = "conc",
+    values_to = "uptake"
+  ) %>% 
+  mutate(
+    conc = str_replace(conc, "X", "") %>% as.numeric()
+  )
+str(test_tidy)
+
+## Compare
+all.equal(test_tidy, CO2, check.attributes = FALSE)
+
+str(CO2)
+str(test_tidy)
+
+levels(CO2$Plant)
+levels(test_tidy$Plant)
