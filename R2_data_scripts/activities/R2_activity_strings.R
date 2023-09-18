20055eab41d07e247bb0f874ffca8f49f2e1916d
Jonathan A. Whiteley <jawhiteley@me.com>
2023-09-18 12:08:00 -0400

    Add activity for strings

---
+++ R2_data_scripts/activities/R2_activity_strings.R
@@ -0,0 +1,38 @@
+################################################################
+### Activity: working with strings using the `stringr` package
+### "A Short Introduction to Working With Data in R"
+### R v4.3.1
+################################################################
+# Libraries ---------------------------------------------------------------
+
+library(dplyr)
+library(stringr)
+
+# LOAD data ---------------------------------------------------------------
+
+data(starwars)  # included in dplyr
+
+
+# Analysis ----------------------------------------------------------------
+
+## 1. Find Star Wars characters with eyes any shade of blue 
+
+
+## 2. Find Star Wars characters with a non-binary sex
+##    (other than "male" or "female", but not missing)
+
+
+## 3. Find Star Wars characters whose Home World starts with "T"
+
+
+## 4. Do any Droid" characters have a name that does *not* contain a "-"?
+
+
+## 5. Make a new object, and replace all "-" characters 
+##    with a comma and space (", ") in the `eye_color` column
+
+
+## 6. Make a new object, with a column named "sex1" that contains 
+##    only the first letter of the values in the `sex` column
+##    - can you make these all upper-case?
+
